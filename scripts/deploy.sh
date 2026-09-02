1fda5e664c1c945df4bb376be61c29e17fd3827e
Igor Bari <mucsi96@gmail.com>
2026-09-02 12:36:20 +0200

    Lower the server CPU request

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -52,7 +52,7 @@ helm upgrade $SERVER_RELEASE_NAME mucsi96/spring-app \
     --set persistentVolumeClaims[0].storageClassName="" \
     --set persistentVolumeClaims[0].storage=5Gi \
     --set resources.requests.memory=512Mi \
-    --set resources.requests.cpu=100m \
+    --set resources.requests.cpu=50m \
     --set resources.limits.memory=1Gi \
     --set resources.limits.cpu=500m \
     --wait
